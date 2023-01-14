497c5f13df73344621898e52ac07737216679294
keksroom <keksroom@gmail.com>
2023-01-14 19:44:54 +0300

    add second view controller

---
+++ loginApp/WelcomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  WelcomeViewController.swift
+//  loginApp
+//
+//  Created by Daniel on 14.01.2023.
+//
+
+import UIKit
+
+class WelcomeViewController: LoginViewController {
+
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func logOutButton() {
+    }
+    
+}
