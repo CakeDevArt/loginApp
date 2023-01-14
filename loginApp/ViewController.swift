0aa4ef227c12b40a810a8ec3945525371f11a913
keksroom <keksroom@gmail.com>
2023-01-14 19:06:57 +0300

    setup project

--- loginApp/ViewController.swift
+++ loginApp/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var userNameTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    let userName = "User"
+    let password = "Password"
+    
+    @IBAction func forgotUserTapped() {
+        showAlert(title: "Ooops!", message: "Your User Name: \(userName)")
     }
-
-
+    @IBAction func forgotPasswordTapped() {
+        showAlert(title: "Ooops!", message: "Your Password: \(password)")
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+        
+    }
+    
 }
 
